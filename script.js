359cabe161fafd0abb82ca16cafb141f3a6f8396
2x5acles <annisacarter123@gmail.com>
2025-04-14 01:54:59 +0000

    x

---
+++ script.js
@@ -0,0 +1,16 @@
+const shoppingCart = [];
+console.log(shoppingCart);
+
+shoppingCart.push('Carrots', 'Celery', 'Asparagus');
+console.log(shoppingCart);
+
+const anything = shoppingCart.pop();
+console.log(anything);
+console.log("Array after popping", shoppingCart);let grocerylist=[];
+grocerylist[0]="Apple"
+grocerylist[1]="Orange"
+grocerylist[2]="Banana"
+grocerylist[3]="Kiwi"
+grocerylist[4]="Mango"
+console.log(grocerylist.length)
+
